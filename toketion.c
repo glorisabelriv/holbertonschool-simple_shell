13b068453bb496ed4a436f03902a50f049ef99ae
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@airdeglorisabel.lan>
2024-04-23 11:42:37 -0400

    contains a function called tokenization that is responsible for dividing a text string (ptr) into tokens using a delimiter

---
+++ toketion.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+/**
+ * tokenization - extract tokens from the getline command
+ *
+ * @ptr: String of the command line
+ * @delim: Delimiter
+ *
+ * Return: the array of tokens
+ */
+char **tokenization(char *ptr, char *delim)
+{
+	char *token = NULL, **tokens = NULL;
+	int i = 0;
+
+	tokens = malloc(sizeof(char *) * 10);
+	token = strtok(ptr, delim);
+
+	while (token)
+	{
+		tokens[i] = malloc(sizeof(char) * _strlen(token) + 1);
+		_strcpy(tokens[i], token);
+		i++;
+		token = NULL;
+		token = strtok(NULL, delim);
+	}
+
+	tokens[i] = NULL;
+	free(token);
+	return (tokens);
+}

edebe85c8f31076818817898ed36a552953f34e1
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@airdeglorisabel.lan>
2024-04-25 13:38:47 -0400

    fixes and changes in the code

--- simple_shell.h
+++ simple_shell.h
@@ -1,5 +1,6 @@
 #ifndef SIMPLE_SHELL_H
 #define SIMPLE_SHELL_H
+#define BUFFER_SIZE 1024
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
@@ -15,10 +16,10 @@ char *add_path(char **tokens, char **tok_path);
 int die(char **tokens);
 
 int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest,char *src);
 int _strcmp(char *compring, char *comp);
 void free_array(char **tokens);
 int _strncmp(char *compring, char *comp, size_t n);
 char *_strdup(char *s);
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest,const char *src);
 #endif

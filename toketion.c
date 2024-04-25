b195b0d37f5c927c98496b0c50642a8e9172b73d
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@airdeglorisabel.lan>
2024-04-25 13:38:41 -0400

    fixes and changes in the code

--- toketion.c
+++ toketion.c
@@ -9,22 +9,36 @@
  */
 char **tokenization(char *ptr, char *delim)
 {
-	char *token = NULL, **tokens = NULL;
-	int i = 0;
+    char *token = NULL, **tokens = NULL;
+    int i = 0, capacity = 10;
 
-	tokens = malloc(sizeof(char *) * 10);
-	token = strtok(ptr, delim);
+    tokens = malloc(sizeof(char *) * capacity);
+    if (!tokens) {
+        perror("malloc failed");
+        return NULL;
+    }
 
-	while (token)
-	{
-		tokens[i] = malloc(sizeof(char) * _strlen(token) + 1);
-		_strcpy(tokens[i], token);
-		i++;
-		token = NULL;
-		token = strtok(NULL, delim);
-	}
-
-	tokens[i] = NULL;
-	free(token);
-	return (tokens);
+    token = strtok(ptr, delim);
+    while (token)
+    {
+        if (i >= capacity - 1) {
+            capacity *= 2;
+            tokens = realloc(tokens, sizeof(char *) * capacity);
+            if (!tokens) {
+                perror("realloc failed");
+                return (NULL);
+            }
+        }
+        tokens[i] = strdup(token);
+        if (!tokens[i]) {
+            perror("strdup failed");
+            free_array(tokens);
+            return (NULL);
+        }
+        i++;
+        token = strtok(NULL, delim);
+    }
+    tokens[i] = NULL;
+    return (tokens);
 }
+

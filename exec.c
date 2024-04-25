2f3f17e44616eeb1718fb7593a80377c26c8f31e
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@airdeglorisabel.lan>
2024-04-25 13:38:36 -0400

    fixes and changes in the code

--- exec.c
+++ exec.c
@@ -4,41 +4,43 @@
  * @tokens: Our array of tokens
  * @env: Our enviorment variables
  * Return: Always return one
- * Description: First our function will compare our token position 0
- * with the built-in (exit & env). If its not a built in he will check
- * for the command and print out the result of the execution.
  */
-int execution(char **tokens,  char **env)
-{
-	int statu;
-	pid_t chid_pid;
-	char *tok_path, **cmd;
-	struct stat buffer;
+#include "simple_shell.h"
 
-	if (stat(tokens[0], &buffer) != 0)
-	{
-		tok_path = path(env);
-		cmd = tokenization(tok_path, ":");
-		free(tok_path);
-		tokens[0] = add_path(tokens, cmd);
-		if (stat(tokens[0], &buffer) != 0)
-		{
-			free_array(cmd);
-			free_array(tokens);
-			perror("cmd not found");
-			return (0);
-		}
-	}
-	chid_pid = fork();
-	if (chid_pid == -1)
-		perror("Child process failed");
-	else if (chid_pid == 0)
-	{
-		if (execve(tokens[0], tokens, env) == -1)
-			perror("./hsh");
-	}
-	else
-		wait(&statu);
-	free_array(tokens);
-	return (1);
+int execution(char **tokens, char **env)
+{
+    int statu;
+    pid_t chid_pid;
+    char *tok_path, **cmd;
+    struct stat buffer;
+
+    if (stat(tokens[0], &buffer) != 0)
+    {
+        tok_path = path(env);
+        cmd = tokenization(tok_path, ":");
+        free(tok_path);
+        if (!cmd) {
+            perror("Failed to tokenize PATH");
+            return (0);
+        }
+        tokens[0] = add_path(tokens, cmd);
+        free_array(cmd);
+        if (!tokens[0] || stat(tokens[0], &buffer) != 0)
+        {
+            perror("cmd not found");
+            return (0);
+        }
+    }
+    chid_pid = fork();
+    if (chid_pid == -1)
+        perror("Child process failed");
+    else if (chid_pid == 0) {
+        execve(tokens[0], tokens, env);
+        perror("./hsh");
+		exit(EXIT_FAILURE);
+    } else {
+        wait(&statu);
+    }
+    free_array(tokens);
+    return (1);
 }

51a70519659c1e05e904796fef858ded6b976f2b
Glorisabel Rivera Rodriguez <glorisabelriverarodriguez@airdeglorisabel.lan>
2024-04-26 00:31:54 -0400

    This function aims to obtain the PATH environment variable of the system. The PATH environment variable

---
+++ path.c
@@ -0,0 +1,18 @@
+#include "simple_shell.h"
+
+/**
+ * path - Function that gets the path from enviorment
+ * @env: Enviorment variables
+ * Return: a string that contains enviorments, or NULL if failes
+ */
+char *path(char **env)
+{
+	int i;
+    for (i = 0; env[i] != NULL; i++) {
+        if (strncmp(env[i], "PATH=", 5) == 0) {
+            char *pathValue = strchr(env[i], '=') + 1;
+            return (strdup(pathValue));
+        }
+    }
+    return (NULL);
+}
